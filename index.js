1a9e3ef9406b8509e4fae836f68f393d4ba052e7
JacquelineMG <j.gilchrist@outlook.com>
2024-05-07 10:00:09 -0700

    implement sumItems function with recursion

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+  for (const a of array) {
+    if (!Array.isArray(a)) {
+      sum += a;
+    } else {
+      sum += sumItems(a);
+    }
+  }
+  return sum;
+
+};
 
 module.exports = sumItems;
